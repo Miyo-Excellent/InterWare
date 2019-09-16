fbae65d7e9a7623164f61f056cd0705939e3bf32
Michell Excellent Marin <theofficesmichell@gmail.com>
2019-09-16 01:52:57 -0500

    update

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const fs = require('fs');
 
 //  App
 const app = express();
@@ -14,7 +15,14 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 //  Statics Files
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../docs')));
+
+app.get('/', (req, res, nex) => {
+  fs.readFile(path.join(__dirname, '../docs/index.html'), null, (error, data) => {
+    if (error) return res.status(200).send('File not fount');
+    return res.status(200).write(data).end()
+  });
+});
 
 app.listen(app.get('port'), () => {
   console.log(`Sever On Port ${app.get('port')}`);
